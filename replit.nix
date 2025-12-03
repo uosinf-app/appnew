289ec150e98fc3bb8145c2b2a1dac58ec410c216
uosinf-app <uosinf7@gmail.com>
2025-12-03 19:07:15 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,7 +1,6 @@
 { pkgs }: {
-    deps = [
-        pkgs.nodejs-18_x
-        pkgs.nodePackages.typescript
-        pkgs.nodePackages.typescript-language-server
-    ];
+  deps = [
+    pkgs.nodejs-20_x
+    pkgs.nodePackages.npm
+  ];
 }
