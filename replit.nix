8a92aa6cee6939b4ecd10a14a76b80b997bd9784
uosinf-app <uosinf7@gmail.com>
2025-12-04 10:50:31 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,7 +1,7 @@
-# النسخة 20.x (موصى بها لأنك تستخدم 20.14.0)
-echo '{ pkgs }: {' > replit.nix
-echo '  deps = [' >> replit.nix
-echo '    pkgs.nodejs-20_x' >> replit.nix
-echo '    pkgs.nodePackages.npm' >> replit.nix
-echo '  ];' >> replit.nix
-echo '}' >> replit.nix
+{pkgs}: {
+  deps = [
+    pkgs.nodejs_20
+    pkgs.nodePackages.npm
+    pkgs.postgresql
+  ];
+}
